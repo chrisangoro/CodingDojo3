3e1287736af986828d0b244ffef7f1339cb5ebe2
felilondocho <pipes.20@hotmail.com>
2015-04-17 22:11:17 -0500

    Receta DTO

--- lobsterLink_core/src/com/eafit/lobsterlink/dto/RecetaDTO.java
+++ lobsterLink_core/src/com/eafit/lobsterlink/dto/RecetaDTO.java
@@ -1,5 +1,33 @@
 package com.eafit.lobsterlink.dto;
 
 public class RecetaDTO {
+	private String nombre;
+	private String ingredientes;
+        private String receta;
 
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getReceta() {
+        return receta;
+    }
+
+    public void setReceta(String receta) {
+        this.receta = receta;
+    }
+        
+        
 }
